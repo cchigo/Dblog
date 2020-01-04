ef42f134cfe4c2ab0cd8955fea9ad87fa9be6758
cchigo <ccasadu15@gmail.com>
2020-01-04 14:18:40 +0100

    create add new layout

--- app/src/main/java/com/example/dblog/MainActivity.kt
+++ app/src/main/java/com/example/dblog/MainActivity.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dblog.adapters.BlogAdapter
 import com.example.dblog.data.Blog
 import com.example.dblog.viewmodels.BlogViewModel
 
@@ -17,13 +20,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        val adapter : BlogAdapter = BlogAdapter()
+        recyclerView.adapter = adapter
+
         //this references the viewmodel
         blogViewModel = ViewModelProviders.of(this).get(BlogViewModel::class.java)
 
         blogViewModel!!.getAllBlogs().observe(this, object : Observer<List<Blog>>{
-            override fun onChanged(t: List<Blog>?) {
+            //updates and refreshes new list
+            override fun onChanged(blogs : List<Blog>?) {
               //update recycler view
-                Toast.makeText(applicationContext, "it works!", Toast.LENGTH_LONG).show()
+                //Toast.makeText(applicationContext, "it works!", Toast.LENGTH_LONG).show()
+            adapter.setBlog(blogs!!)
+
             }
 
         })
---
+++ app/src/main/java/com/example/dblog/adapters/BlogAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.dblog.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dblog.R
+import com.example.dblog.data.Blog
+import kotlinx.android.synthetic.main.blog_item.view.*
+
+class BlogAdapter: RecyclerView.Adapter<BlogAdapter.BlogHolder>() {
+    private var blogs: List<Blog> = ArrayList<Blog>()
+
+    //holds views in each recyclerview item
+
+    class BlogHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var textViewTitle : TextView = itemView.text_view_title
+       var textViewCategory : TextView = itemView.text_view_category
+        var textViewContent : TextView = itemView.text_view_content
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogHolder {
+       val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.blog_item, parent, false)
+        return BlogHolder(itemView)
+    }
+
+        //takes care of getting data from blog object into the views(title, category and content) of note holder
+    override fun onBindViewHolder(holder: BlogHolder, position: Int) {
+            val currentBlog: Blog = blogs[position]
+
+            holder.textViewTitle.text = currentBlog.title
+            holder.textViewCategory.text = currentBlog.category
+            holder.textViewContent.text = currentBlog.content
+
+    }
+
+    //sets lists of notes in recycler view
+    fun setBlog(blogs: List<Blog> ){
+        this.blogs = blogs
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return blogs.size
+    }
+}
