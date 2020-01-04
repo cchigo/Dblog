bb5c6a5c5fb31eeee7582d5872006d7162da10a9
cchigo <ccasadu15@gmail.com>
2020-01-04 09:14:11 +0100

    create blog database

---
+++ app/src/main/java/com/example/dblog/data/BlogDatabase.kt
@@ -0,0 +1,60 @@
+package com.example.dblog.data
+
+import android.content.Context
+import android.os.AsyncTask
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+@Database(entities = [Blog::class], version = 1)
+abstract class BlogDatabase: RoomDatabase() {
+
+    abstract fun blogDao(): BlogDao
+
+    companion object {
+        private var instance: BlogDatabase? = null
+
+
+        fun getInstance(context: Context): BlogDatabase? {
+            if (instance == null) {
+                synchronized(BlogDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        BlogDatabase::class.java, "note_database"
+                    )
+                        .fallbackToDestructiveMigration() // when version increments, it migrates (deletes db and creates new) - else it crashes
+                        .addCallback(roomCallback)
+                        .build()
+                }
+            }
+            return instance
+        }
+
+        fun destroyInstance() {
+            instance = null
+        }
+
+        private val roomCallback = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulateDbAsyncTask(instance)
+                    .execute()
+            }
+        }
+
+    }
+    class PopulateDbAsyncTask(db: BlogDatabase?) : AsyncTask<Unit, Unit, Unit>() {
+        private val blogDao = db?.blogDao()
+
+        override fun doInBackground(vararg params: Unit?) {
+            blogDao?.insert(Blog("category1", "title1", "content1"))
+            blogDao?.insert(Blog("category2", "title2", "content2"))
+            blogDao?.insert(Blog("category3", "title3", "content3"))
+            blogDao?.insert(Blog("category4", "title4", "content4"))
+            blogDao?.insert(Blog("category5", "title5", "content5"))
+            blogDao?.insert(Blog("category6", "title6", "content6"))
+            }
+    }
+
+}
