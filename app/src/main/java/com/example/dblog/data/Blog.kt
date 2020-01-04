1b39172419848971fc5df6c3ee1fb73ac223e9fe
cchigo <ccasadu15@gmail.com>
2020-01-04 08:49:23 +0100

    create Blog data class

---
+++ app/src/main/java/com/example/dblog/data/Blog.kt
@@ -0,0 +1,16 @@
+package com.example.dblog.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "blog_table")
+data class Blog(
+    var category: String,
+
+    var title: String,
+
+    var content: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
