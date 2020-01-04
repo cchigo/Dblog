725c8a4dfde124e740bca90709e36520a34e197f
cchigo <ccasadu15@gmail.com>
2020-01-04 09:03:38 +0100

    create Blog DAO

---
+++ app/src/main/java/com/example/dblog/data/BlogDao.kt
@@ -0,0 +1,23 @@
+package com.example.dblog.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface BlogDao {
+
+    @Insert
+    fun insert(blog: Blog)
+
+    @Update
+    fun update(blog: Blog)
+
+    @Delete
+    fun delete(blog: Blog)
+
+    @Query("DELETE FROM blog_table")
+    fun deleteAllBlogs(blog: Blog)
+
+    @Query("SELECT * FROM blog_table")
+    fun getAllNotes(): LiveData<List<Blog>>
+}
